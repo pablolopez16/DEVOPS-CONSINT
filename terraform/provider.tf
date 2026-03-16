84f5b82b8a87bd829d071cbcb6a2bd59aec0d5b7
Pablo Lopez <pablolopez161616@gmail.com>
2026-03-16 19:20:37 +0100

    terraform necesita credenciales aunqeu sean falsas para ejecuytarse

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   region                      = "eu-south-2"
+  access_key                  = "test"
+  secret_key                  = "test"
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
 
   endpoints {
     s3  = "http://localhost:4566"
