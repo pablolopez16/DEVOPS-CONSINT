782eeaaf3f15ceb1b1249a395213ec55f155098b
Pablo Lopez <pablolopez161616@gmail.com>
2026-03-17 11:48:16 +0100

    terraform fmt test

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ data "aws_iam_policy_document" "amzn-policy-queue" {
     effect = "Allow"
 
     principals {
-      type        = "service"   //aws, service,...
-      identifiers = ["*"] // who in the type we filter
+      type        = "service" //aws, service,...
+      identifiers = ["*"]     // who in the type we filter
 
     }
 
